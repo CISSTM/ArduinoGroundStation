9121722c2b26916ae101d976b89d3231069b43b1
michadenheijer <micha@michadenheijer.com>
2019-12-04 18:47:09 +0100

    Remove Pi files, fix others

--- ground_station/ground_station.ino
+++ ground_station/ground_station.ino
@@ -1,5 +1,7 @@
 #include <SPI.h>
 #include <RH_RF69.h>
+#include <SoftwareSerial.h>
+#include <math.h>
 
 #define RF69_FREQ 868.0
 
@@ -8,12 +10,14 @@
 #define RFM69_RST     2
 
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
+SoftwareSerial mySerial(9, 10);
 
 int16_t packetnum = 0;
 
 void setup() 
 {
   Serial.begin(9600);
+  mySerial.begin(9600);
    
   pinMode(RFM69_RST, OUTPUT);
   digitalWrite(RFM69_RST, LOW);
@@ -34,7 +38,6 @@ void setup()
   }
 
   rf69.setTxPower(20, true);
-  pinMode(LED, OUTPUT);
 }
 
 
@@ -45,9 +48,24 @@ void loop() {
     if (rf69.recv(buf, &len)) {
       if (!len) return;
       buf[len] = 0;
-      Serial.println(rf69.lastRssi(), DEC);
+      Serial.print("Distance");
+      Serial.println(getDistanceRSSI(rf69.lastRssi(), 868000000.0, 2.0, 1), DEC);
     } else {
       Serial.println("Receive failed");
     }
   }
+  if(mySerial.available() > 1){
+    String input = mySerial.readString();
+    Serial.println(input);    
+  }
+  delay(10);
+}
+
+float getDistanceRSSI(float signal_loss, float freq, float signal_decay, float distance_0) {
+    float pi = 3.14159;
+    float lightspeed = 299792458;
+    float golflength =  lightspeed / freq;
+    float distance = distance_0 * powf(10, (signal_loss/(10*signal_decay))) * powf((golflength/(4*pi*distance_0)), (2/signal_decay));
+
+    return distance;
 }
